c14a7d105d1016bc2a4a17d5be9bb8275175cb08
hieuvn8 <HieuVN8@fpt.com>
2025-07-21 15:48:34 +0700

    Initial commit, connect db

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log("Server is running on Port: " + PORT);
+  connectDB();
+});
