1fa0329c7d1f1d65f057459ec97ae7caae1bf7d6
hieuvn8 <HieuVN8@fpt.com>
2025-07-23 15:23:15 +0700

    update config to add image with large size

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
